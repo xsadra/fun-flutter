c35ad17bca8ce690e49b0781a6938f5d8c29b1bc
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2022-12-14 10:00:28 +0100

    [riverpod2] Add riverpod and use it to count

--- Riverpod/ResoCoder/riverpod2/lib/widget/counter_page.dart
+++ Riverpod/ResoCoder/riverpod2/lib/widget/counter_page.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod2/provider/app_provider.dart';
 
 class CounterPage extends ConsumerWidget {
   const CounterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
       ),
       body: Center(
         child: Text(
-          '0',
+          counter.toString(),
           style: Theme.of(context).textTheme.displayMedium,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
         child: const Icon(Icons.add),
       ),
     );
