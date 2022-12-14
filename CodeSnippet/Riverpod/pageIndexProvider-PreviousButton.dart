12d7c06d6a684c477a8373efb7b0a69cc0ac1b12
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2022-12-14 17:10:23 +0100

    [CodeSnippet] pageIndexProvider - Previous Button

---
+++ CodeSnippet/Riverpod/pageIndexProvider-PreviousButton.dart
@@ -0,0 +1,26 @@
+final pageIndexProvider = StateProvider<int>((ref) => 0);
+
+// A provider which computes whether the user is allowed to go to the previous page
+final canGoToPreviousPageProvider = Provider<bool>((ref) {
+  return ref.watch(pageIndexProvider) != 0;
+});
+
+class PreviousButton extends ConsumerWidget {
+  const PreviousButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // We are now watching our new Provider
+    // Our widget is no longer calculating whether we can go to the previous page.
+    final canGoToPreviousPage = ref.watch(canGoToPreviousPageProvider);
+
+    void goToPreviousPage() {
+      ref.read(pageIndexProvider.notifier).update((state) => state - 1);
+    }
+
+    return ElevatedButton(
+      onPressed: canGoToPreviousPage ? goToPreviousPage : null,
+      child: const Text('previous'),
+    );
+  }
+}
