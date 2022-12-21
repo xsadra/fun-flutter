5c06e679abf93e836b5ac457edb997f2b7b691d2
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2022-12-21 10:43:09 +0100

    [CodeSnippet][CustomOperators] Multiplying an Iterable

--- CodeSnippet/Dart/custom-operators.dart
+++ CodeSnippet/Dart/custom-operators.dart
@@ -5,7 +5,11 @@ void main(List<String> args) {
   final you = FamilyMember(name: 'Sara');
   final family = me + you;
   print(family);
-  // Part 01 - END
+
+  // Part 02 -  Multiplying an Iterable
+  const numbers = ['one', 'two', 'three'];
+  final result = numbers * 3;
+  print(result);
 }
 
 // Part 01 - Add to Same class together
@@ -37,3 +41,13 @@ extension ToFamily on FamilyMember {
       );
 }
 // Part 01 - END
+
+// Part 02 -  Multiplying an Iterable
+extension Times<T> on Iterable<T> {
+  Iterable<T> operator *(int times) sync* {
+    for (var i = 0; i < times; i++) {
+      yield* this;
+    }
+  }
+}
+// Part 02 - END
