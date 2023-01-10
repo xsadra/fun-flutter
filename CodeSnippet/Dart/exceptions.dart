a0facafe75a605468445e540cbff38f790e437ff
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-10 12:42:36 +0100

    [CodeSnippet][Exceptions] Custom Throws Annotation

--- CodeSnippet/Dart/exceptions.dart
+++ CodeSnippet/Dart/exceptions.dart
@@ -1,5 +1,5 @@
 // Exceptions and Errors
-void main(List<String> args) {
+void main(List<String> args) async {
   // Part 01 - Throwing in Class Constructors
   tryCreatingPerson(age: 20);
   tryCreatingPerson(age: -1);
@@ -31,6 +31,25 @@ void main(List<String> args) {
   }
 
   // Part 05 - Custom Throws Annotation
+  try {
+    print('Dog aged 11 is going to run...');
+    Dog5(
+      age: 11,
+      isTired: false,
+    ).run();
+  } catch (e) {
+    print(e);
+  }
+
+  try {
+    print('A tired dog is going to run...');
+    Dog5(
+      age: 2,
+      isTired: true,
+    ).run();
+  } catch (e) {
+    print(e);
+  }
 
   // Part 06 - Throwing Errors
 
@@ -175,6 +194,54 @@ class DatabaseNotOpenException implements Exception {
 // Part 04 - END
 
 // Part 05 - Custom Throws Annotation
+class Throws<T> {
+  final List<T> exceptions;
+  const Throws(this.exceptions);
+}
+
+class DogIsTooOldException implements Exception {
+  const DogIsTooOldException();
+}
+
+class DogIsTiredException implements Exception {
+  const DogIsTiredException();
+}
+
+class Animal {
+  final int age;
+
+  const Animal({
+    required this.age,
+  });
+
+  @Throws([UnimplementedError])
+  void run() => throw UnimplementedError();
+}
+
+class Dog5 extends Animal {
+  final bool isTired;
+
+  const Dog5({
+    required super.age,
+    required this.isTired,
+  });
+
+  @Throws([DogIsTooOldException, DogIsTiredException])
+
+  /// This function throws the following exceptions:
+  /// - [DogIsTooOldException] if the dog is older than 10 years old.
+  /// - [DogIsTiredException] if the dog is tired.
+  @override
+  void run() {
+    if (age > 10) {
+      throw const DogIsTooOldException();
+    } else if (isTired) {
+      throw const DogIsTiredException();
+    } else {
+      print('Dog is running');
+    }
+  }
+}
 // Part 05 - END
 
 // Part 06 - Throwing Errors
