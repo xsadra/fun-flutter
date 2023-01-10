14fabc5811bedb0b152b7e1ff8da2e0096e15bd9
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-10 17:08:41 +0100

    [Riverpod2x][Example1] Using Provider to show time

--- Riverpod/Vandad/Riverpod2.x/example1/lib/main.dart
+++ Riverpod/Vandad/Riverpod2.x/example1/lib/main.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: true,
       home: const HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+final currentDate = Provider<DateTime>((ref) => DateTime.now());
+
+class HomePage extends ConsumerWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Material App Bar'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Center(
+        child: Text(
+          date.toIso8601String(),
+          style: Theme.of(context).textTheme.headline4,
+        ),
       ),
     );
   }
