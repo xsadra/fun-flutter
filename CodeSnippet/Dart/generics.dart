569cfdb96b2eb32168fbd06ee76d1dba771b8878
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-10 08:58:50 +0100

    [CodeSnippet][Generics] Generic Function Pointers

--- CodeSnippet/Dart/generics.dart
+++ CodeSnippet/Dart/generics.dart
@@ -78,6 +78,10 @@ void main(List<String> args) {
   print(animalName);
 
   // Part 11 - Generic Function Pointers
+  const integers = [100, 5, 2];
+  const doubles = [33.0, 3, 2.0];
+  print(integers.reduce(divide));
+  print(doubles.reduce(divide));
 
   // Part 12 - Generic Class Properties
 }
@@ -275,7 +279,13 @@ extension<T> on T {
 // Part 10 - END
 
 // Part 11 - Generic Function Pointers
-
+T divide<T extends num>(T lhs, T rhs) {
+  if (lhs is int && rhs is int) {
+    return lhs ~/ rhs as T;
+  } else {
+    return lhs / rhs as T;
+  }
+}
 // Part 11 - END
 
 // Part 12 - Generic Class Properties
