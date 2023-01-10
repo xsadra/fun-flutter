48b9ef3d563c0b6a1616ae1715e09d1b8513ba11
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-10 08:48:23 +0100

    [CodeSnippet][Generics] Generic Sorting with Comparable

--- CodeSnippet/Dart/generics.dart
+++ CodeSnippet/Dart/generics.dart
@@ -53,6 +53,8 @@ void main(List<String> args) {
   print(swapped);
 
   // Part 08 -  Generic Sorting with Comparable
+  sort(ascending: false);
+  sort(ascending: true);
 
   // Part 09 - Handling Lack of Common Ancestors
 
@@ -167,7 +169,19 @@ extension<L extends num, R extends num> on Tuple<L, R> {
 // Part 07 - END
 
 // Part 08 -  Generic Sorting with Comparable
+const ages = [100, 20, 10, 90, 40];
+const names = ['Foo', 'Bar', 'Baz'];
+const distances = [3.1, 10.2, 1.3, 4.2];
 
+int isLessThan<T extends Comparable>(T a, T b) => a.compareTo(b);
+int isGreaterThan<T extends Comparable>(T a, T b) => b.compareTo(a);
+
+void sort({required bool ascending}) {
+  final comparator = ascending ? isLessThan : isGreaterThan;
+  print([...ages]..sort(comparator));
+  print([...names]..sort(comparator));
+  print([...distances]..sort(comparator));
+}
 // Part 08 - END
 
 // Part 09 - Handling Lack of Common Ancestors
