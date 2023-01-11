54fb39044b0f5f760565d3aa3522dce83bb45648
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-11 13:06:55 +0100

    [Riverpod2x][Example2] Use StateNotifierProvider to provide a StateNotifier class

--- Riverpod/Vandad/Riverpod2.x/example2/lib/main.dart
+++ Riverpod/Vandad/Riverpod2.x/example2/lib/main.dart
@@ -1,33 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final local = this;
+    if (local != null) {
+      return local + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
+);
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: true,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Material App Bar'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer(
+              builder: (context, ref, child) {
+                final counter = ref.watch(counterProvider);
+                return Text(
+                  counter == null ? 'Increase the Number' : counter.toString(),
+                  style: const TextStyle(fontSize: 30),
+                );
+              },
+            ),
+            TextButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).increment();
+              },
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
       ),
     );
   }
