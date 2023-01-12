8b946fbbaa7a4181bd9e18ac6dc8ae7280a94654
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-12 10:34:46 +0100

    [Riverpod2x][Example6] Create a new project

---
+++ Riverpod/Vandad/Riverpod2.x/example6/android/app/src/main/kotlin/at/sadra/tutorial/riverpod2X/example6/MainActivity.kt
@@ -0,0 +1,6 @@
+package at.sadra.tutorial.riverpod2X.example6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Riverpod/Vandad/Riverpod2.x/example6/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: Center(),
+    );
+  }
+}
