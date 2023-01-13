138b454e69c2756557ab890d638df8ffb726bfcb
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-13 11:46:19 +0100

    [Riverpod2x][PicoGram] Setup the firebase and add other dependencies

--- Riverpod/Vandad/Riverpod2.x/picogram/lib/main.dart
+++ Riverpod/Vandad/Riverpod2.x/picogram/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(
-      const ProviderScope(
-        child: MyApp(),
-      ),
-    );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
