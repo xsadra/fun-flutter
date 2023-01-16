db95c2b5083ac145e7370341e406a1aa49fc414d
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-01-16 16:20:44 +0100

    [Riverpod2x][PicoGram] Create strings object to store app strings

---
+++ Riverpod/Vandad/Riverpod2.x/picogram/lib/views/components/constants/strings.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+class Strings {
+  static const String allowLikesTitle = 'Allow likes';
+  static const String allowLikesDescription =
+      'Allow other users to like your posts';
+  static const String allowLikesKey = 'allow_likes';
+  static const String allowCommentsTitle = 'Allow comments';
+  static const String allowCommentsDescription =
+      'Allow other users to comment on your posts';
+  static const String allowCommentsStorageKey = 'allow_comments';
+  static const String comment = 'comment';
+  static const String loading = 'Loading...';
+  static const String person = 'person';
+  static const String people = 'people';
+  static const String likedThis = 'liked this';
+  static const String delete = 'Delete';
+  static const String areYouSureYouWantToDeleteThis =
+      'Are you sure you want to delete this?';
+  static const String logout = 'Logout';
+  static const String areYouSureThatYouWantToLogOutOfTheApp =
+      'Are you sure that you want to log out of the app?';
+  static const String cancel = 'Cancel';
+
+  const Strings._();
+}
