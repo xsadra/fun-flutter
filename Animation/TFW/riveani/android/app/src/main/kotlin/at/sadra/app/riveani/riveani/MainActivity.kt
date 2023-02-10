e6353f674040813bfb926d493f2a21bdf672babd
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-02-10 11:43:52 +0100

    [Animation][TFW][ARive] Initial the project

---
+++ Animation/TFW/riveani/android/app/src/main/kotlin/at/sadra/app/riveani/riveani/MainActivity.kt
@@ -0,0 +1,6 @@
+package at.sadra.app.riveani.riveani
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Animation/TFW/riveani/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'screens/onboading/onboading_screen.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rive Animation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFEEF1F8),
+        primarySwatch: Colors.blue,
+        fontFamily: "Intel",
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
+        ),
+      ),
+      home: const OnboardingScreen(),
+    );
+  }
+}
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color(0xFFDEE3F2),
+    width: 1,
+  ),
+);
