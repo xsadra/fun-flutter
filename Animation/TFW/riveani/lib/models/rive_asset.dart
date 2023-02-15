d4fa5042296ad08982e639702f2cfefa3f37538e
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-02-15 15:07:42 +0100

    [Animation][TFW][ARive] Create RiveAsset to hold bottom nav bar buttons data

---
+++ Animation/TFW/riveani/lib/models/rive_asset.dart
@@ -0,0 +1,56 @@
+import 'package:rive/rive.dart';
+
+import '../constants.dart';
+
+class RiveAsset {
+  RiveAsset({
+    required this.artboard,
+    required this.stateMachineName,
+    required this.title,
+    required this.src,
+    this.input,
+  });
+
+  final String artboard;
+  final String stateMachineName;
+  final String title;
+  final String src;
+  late SMIBool? input;
+
+  set setInput(SMIBool status) {
+    input = status;
+  }
+}
+
+List<RiveAsset> bottomNavAssets = [
+  RiveAsset(
+    artboard: 'CHAT',
+    stateMachineName: 'CHAT_Interactivity',
+    title: 'Chat',
+    src: Assets.iconsRive,
+  ),
+  RiveAsset(
+    artboard: 'SEARCH',
+    stateMachineName: 'SEARCH_Interactivity',
+    title: 'Search',
+    src: Assets.iconsRive,
+  ),
+  RiveAsset(
+    artboard: 'TIMER',
+    stateMachineName: 'TIMER_Interactivity',
+    title: 'Timer',
+    src: Assets.iconsRive,
+  ),
+  RiveAsset(
+    artboard: 'BELL',
+    stateMachineName: 'BELL_Interactivity',
+    title: 'Bell',
+    src: Assets.iconsRive,
+  ),
+  RiveAsset(
+    artboard: 'USER',
+    stateMachineName: 'USER_Interactivity',
+    title: 'User',
+    src: Assets.iconsRive,
+  ),
+];
