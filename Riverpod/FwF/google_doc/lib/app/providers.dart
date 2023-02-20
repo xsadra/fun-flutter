92fa5bb6a03891aeb9569afc886d15b16ea552fa
Sadra Babai (ac) <sadra.babai@artichoke.eu>
2023-02-20 12:27:47 +0100

    [Riverpod][FwF][G-Doc] Add app Providers

---
+++ Riverpod/FwF/google_doc/lib/app/providers.dart
@@ -0,0 +1,27 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_doc/app/constants.dart';
+
+abstract class Dependency {
+  static Provider<Client> get client => _clientProvider;
+  static Provider<Account> get account => _accountProvider;
+  static Provider<Databases> get database => _databaseProvider;
+  static Provider<Realtime> get realtime => _realtimeProvider;
+}
+
+final _clientProvider = Provider<Client>((ref) => Client()
+  ..setProject(appwriteProjectId)
+  ..setSelfSigned(status: true)
+  ..setEndpoint(appwriteEndpoint));
+
+final _accountProvider = Provider<Account>(
+  (ref) => Account(ref.read(_clientProvider)),
+);
+
+final _databaseProvider = Provider<Databases>(
+  (ref) => Databases(ref.read(_clientProvider)),
+);
+
+final _realtimeProvider = Provider<Realtime>(
+  (ref) => Realtime(ref.read(_clientProvider)),
+);
